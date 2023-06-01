61e2bde14f29bc33d86d905545ad77728f108920
gyojinK <gyojink@gyojinKui-MacBookAir.local>
2023-06-01 17:34:30 +0900

    뷰 연결 작업중

--- app/src/main/java/com/example/my_time_trip/MainActivity.java
+++ app/src/main/java/com/example/my_time_trip/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.my_time_trip;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRv_todo;
+    private FloatingActionButton mBtn_write;
+    private ArrayList<TodoItemVO> mTodoItems;
+    private DBHelper mDBHelper; // m은 멤버라는 의미 = 전역변수
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setInit();
+    }
+
+    private void setInit() {
+        // 초기화 작업
+        mDBHelper = new DBHelper(this);
+        mRv_todo = findViewById(R.id.rv_todo);
+        mBtn_write = findViewById(R.id.btn_write);
+        mTodoItems = new ArrayList<>();
+
+        mBtn_write.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Dialog는 팝업창
+                Dialog dialog = new Dialog(MainActivity.this, android.R.style.Theme_Material_Light_Dialog);
+                dialog.setContentView(R.layout.dialog_edit);
+            }
+        });
+
     }
 }
