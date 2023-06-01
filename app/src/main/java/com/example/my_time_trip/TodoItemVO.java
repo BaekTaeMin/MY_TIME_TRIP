f1706425093a80fa7222f5074ea046bfabeef666
gyojinK <gyojink@gyojinKui-MacBookAir.local>
2023-06-01 17:34:49 +0900

    데이터VO 생성

--- app/src/main/java/com/example/my_time_trip/TodoItemVO.java
+++ app/src/main/java/com/example/my_time_trip/TodoItemVO.java
@@ -1,2 +1,42 @@
-package com.example.my_time_trip;public class TodoItem {
+package com.example.my_time_trip;
+
+public class TodoItemVO {
+    private int cno;
+    private String title;
+    private String content;
+    private String toDate;
+
+    public TodoItemVO(){};
+
+    public int getCno() {
+        return cno;
+    }
+
+    public void setCno(int cno) {
+        this.cno = cno;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getToDate() {
+        return toDate;
+    }
+
+    public void setToDate(String toDate) {
+        this.toDate = toDate;
+    }
 }
